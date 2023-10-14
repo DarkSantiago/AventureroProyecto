297a8f4a2e2a442e8c98774046a90e4cf6d1058c
DarkSantiago <dasa.osoriop@gmail.com>
2023-10-14 00:25:14 -0500

    SegundoGuardado

---
+++ Adventure.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int i,j;
+    char Matriz[40][40];
+
+    for(i = 0; i < 40; i++)
+    {
+        Matriz[0][i] = '*';
+        Matriz[39][i] = '*';
+    }
+    for(i = ; i < 40; i++)
+    {
+        Matriz[0][i]
+    }
+
+
+    for( i = 0; i < 40; i++)
+    {
+        printf("%c", Matriz[0][i]);
+
+
+    }
+
+}
